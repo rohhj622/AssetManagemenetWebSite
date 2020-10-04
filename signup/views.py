3aa2151cd7e33ec343191e3e024448d4349cbb18
HyunJin <rohhj622@gmail.com>
2020-10-04 21:05:43 +0900

    회원가입기능만 구현:201004

--- signup/views.py
+++ signup/views.py
@@ -7,35 +7,23 @@ from .models import *
 
 # Create your views here.
 
-def signupMain(request):
+def signup(request):
     if request.method == 'GET':
         return render(request, 'signup/signUpMain.html')
 
-
-def setRate(request):
     if request.method == "POST":
-        n_m_id = request.POST.get('m_id')
-        n_m_pw = request.POST.get('m_pw')
-        n_m_name = request.POST.get('m_name')
-        print(n_m_name,n_m_id,n_m_pw)
-
-        new_mem = {'m_id': n_m_id, 'm_pw': n_m_pw, 'm_name': n_m_name}
-
-        return render(request, 'signup/setRate.html', new_mem)
-
-
-def registMem(request):
-    if request.method == 'POST':
         m_id = request.POST.get('m_id')
         m_pw = request.POST.get('m_pw')
         m_name = request.POST.get('m_name')
         m_sdrate = request.POST.get('m_sdrate')
         m_srate = request.POST.get('m_srate')
         m_frate = request.POST.get('m_frate')
+        print(m_name, m_id, m_pw)
 
         new_mem = Member(m_id=m_id, m_pw=m_pw, m_name=m_name,
                          m_sdrate=m_sdrate, m_srate=m_srate, m_frate=m_frate)
 
         # db insert
         new_mem.save()
-        return HttpResponse('hellpo')
+
+        return render(request, 'signup/setRate.html')
